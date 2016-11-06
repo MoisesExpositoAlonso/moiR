eeb997862803536eef9a0bd6b2df8afa0016b0c3
mexposito <moises.exposito-alonso@tuebingen.mpg.de>
2016-11-06 21:20:37 +0100

    Corrected DESCRIPTION. It had black line at the end!

--- R/evalparse.R
+++ R/evalparse.R
@@ -1,12 +1,7 @@
 #' Wrap of functions eval and parse for less code writing
 #' 
 #' @param the function with the required arguments given as a single string
-#'
-#' @return runs the passed function.
-#'
-#' @details
 #' @importFrom base eval
-#' @export
 #' @return
 #' Returns whatever the function with its given arguments will return.
 #'
