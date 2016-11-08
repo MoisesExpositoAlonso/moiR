bb5a29b3a91967435bde92eed582e1188b19f1a1
mexposito <moises.exposito-alonso@tuebingen.mpg.de>
2016-11-08 14:35:49 +0100

    improvede envir_plot docs

--- R/envir_plot.R
+++ R/envir_plot.R
@@ -1,45 +1,41 @@
 #' Pretty plot of raster object
 #' 
 #' @parameter raster A raster layer object
-#' @parameter objectname a numeric vector of group 2
-#' @parameter name 
-#' @parameter dimcol
-#' @parameter vecol
-#' @parameter rangecol
-#' @parameter pdf
-#' @parameter numbreak
-#' @parameter contrast
-#' @parameter method string, either t.student (parametric) or wilcox.test (nonparametric)
-#' @parameter title string with the name of the plot
-#' @parameter wantpoints Logical FALSE/TRUE to determine whether points will be overplotted
-#' @parameter wantviolin Logical FALSE/TRUE to determine whether you want a violine plot in the background
-#' @parameter The colors of the two groups. Default c("darkorchid4","aquamarine4")
+#' @parameter objectname The name of the object plotted or other name to keep track of the pdf
+#' @parameter name Title fo the plot
+#' @parameter dimcol Number of color breaks in the gradient
+#' @parameter vecol Color vector for the palette
+#' @parameter breaks Breaks used in the color scale
+#' @parameter rangecol Range of the color palette
+#' @parameter numbreak Number of breaks in the color scale
+#' @parameter contrast Interger for number of increase contrast iteration
+#' @parameter pdf Logical whether to output plot as pedf
+#' @parameter add Logical whether this plot will be overplotted on an existing one in the graphic device
 #'
-#' @return Prints a plot with the given test p-value. If title is provided, it prints into a pdf.
+#' @return Prints a plot of a raster object with a nice color palette and without axis (easily changed). If desired, it is outputed as pdf.
+#'
 #' @examples
-#' group1=rnorm(n = 100,mean = 1,sd = 1)
-#' group1=rnorm(n = 100,mean = 2,sd = 1)
-#' thetimenow<-contrast_test(group1,group2)
+#' 
+#' 
+#' 
 #' @export
 #' 
-envir_plot<-function(raster,objectname=deparse(substitute(raster)),dimcol=100,name="",vecol=NULL,breaks=format(seq(rangecol[1],rangecol[2],len=numbreak),digits = 1)
-,rangecol=c(min(values(raster),na.rm = T),max(values(raster),na.rm = T) ),numbreak=5,contrast=0,pdf=F){
+envir_plot<-function(raster,objectname=deparse(substitute(raster)),name="",dimcol=100,vecol=c('cyan','dodgerblue1','blue','forestgreen','orange','red','purple'),
+  breaks=format(seq(rangecol[1],rangecol[2],len=numbreak),digits = 1),
+  rangecol=c(min(values(raster),na.rm = T),max(values(raster),na.rm = T) ),numbreak=5,contrast=0,pdf=F, add=F){
 library(raster)
 
 if(pdf == T ){ write.pdf(paste0("plots/raster_",objectname,"_",name,".pdf")) }
 
 # Generate color
-if( is.null(vecol ) ){
-mypalette<-make.pallete.contrast(c('cyan','dodgerblue1','blue','forestgreen','orange','red','purple'),contrast)
-}else{
 mypalette<-make.pallete.contrast(vecol)
-}
+
 
 # Generate sequence of breaks in legend 
 arg <- list(at=breaks)
 
 # Plot 
-plot(raster, xaxt="n", yaxt="n", main="", col=mypalette(dimcol), axis.args=arg,axes=F,box=F,frame.plot=F,bty="n")
+plot(raster, xaxt="n", yaxt="n", main="", col=mypalette(dimcol), axis.args=arg,axes=F,box=F,frame.plot=F,bty="n",add=add)
 # plot(raster, xaxt="n", yaxt="n", main="", col=mypalette(dimcol), breaks=mysequence,axis.args=arg,axes=F,box=F,frame.plot=F,bty="n")
 # box()
 # axis(1,las=1)
--- R/transparent.R
+++ R/transparent.R
@@ -1,8 +1,11 @@
 #' Make a color transparent
 #' This function applies the level of transparency desired for a color or vector of colors
+#'
 #' @parameter color HEX code or name.
 #' @parameter alpha Numerical between 0 and 1. 
+#'
 #' @return The color HEX after applying a number of transparency
+#'
 #' @examples
 #' blacktransparent<-transparent("black",alpha=0.5)
 #' 
