5af403e4eca4caa9ac335b3c6a085297bfe10c3f
mexposito <moises.exposito-alonso@tuebingen.mpg.de>
2016-11-09 10:57:44 +0100

    added jet colors

--- R/envirplot.R
+++ R/envirplot.R
@@ -11,8 +11,8 @@
 #' @parameter contrast Interger for number of increase contrast iteration
 #' @parameter pdf Logical whether to output plot as pedf
 #' @parameter add Logical whether this plot will be overplotted on an existing one in the graphic device
-#'
-#' @return Prints a plot of a raster object with a nice color palette and without axis (easily changed). If desired, it is outputed as pdf.
+#' @parameter plotlegend Logical whether the color legend is plotted. Useful when overlaying two maps.
+#' @return Prints a plot of a raster object with a nice color palette and without axis (easily changed). If desired, it is outputed as pdf'.
 #'
 #' @examples
 #' 
@@ -22,7 +22,7 @@
 #' 
 envirplot<-function(raster,objectname=deparse(substitute(raster)),name="",dimcol=100,vecol=c('cyan','dodgerblue1','blue','forestgreen','orange','red','purple'),
   breaks=format(seq(rangecol[1],rangecol[2],len=numbreak),digits = 1),
-  rangecol=c(min(values(raster),na.rm = T),max(values(raster),na.rm = T) ),numbreak=5,contrast=0,pdf=F, add=F){
+  rangecol=c(min(values(raster),na.rm = T),max(values(raster),na.rm = T) ),numbreak=5,mysequence=NULL,midpoint=NULL,contrast=0,pdf=F, add=F,plotlegend=T,discrete=F){
 library(raster)
 
 if(pdf == T ){ write.pdf(paste0("plots/raster_",objectname,"_",name,".pdf")) }
@@ -31,10 +31,27 @@ if(pdf == T ){ write.pdf(paste0("plots/raster_",objectname,"_",name,".pdf")) }
 mypalette<-make.pallete.contrast(vecol)
 
 # Generate sequence of breaks in legend 
+if(!is.null(mysequence)){
+if(is.null(midpoint)){
+mysequence<- seq(rangecol[1],rangecol[2],len = dimcol-1)
+}
+else{
+	if( midpoint <rangecol[1] | midpoint > rangecol[2] ) { stop("Error: midpoint must be within the range of your data!")
+	}
+mysequence<- c(seq(rangecol[1],midpoint,len = round(dimcol/2 )  ), seq(midpoint,rangecol[2],len = round(dimcol/2 )  ) [-1]  )
+}
+}
 arg <- list(at=breaks)
 
 # Plot 
-plot(raster, xaxt="n", yaxt="n", main="", col=mypalette(dimcol), axis.args=arg,axes=F,box=F,frame.plot=F,bty="n",add=add)
+if(discrete==T){
+plot(raster, xaxt="n", yaxt="n", main="", col=vecol, axes=F,box=F,frame.plot=F,bty="n",add=add,legend=plotlegend)
+
+}else{
+plot(raster, xaxt="n", yaxt="n", main="", col=mypalette(dimcol), axis.args=arg,breaks=mysequence,axes=F,box=F,frame.plot=F,bty="n",add=add,legend=plotlegend)
+
+}
+# plot(raster, xaxt="n", yaxt="n", main="", col=mypalette(dimcol), axis.args=arg,axes=F,box=F,frame.plot=F,bty="n",add=add,legend=plotlegend)
 # plot(raster, xaxt="n", yaxt="n", main="", col=mypalette(dimcol), breaks=mysequence,axis.args=arg,axes=F,box=F,frame.plot=F,bty="n")
 # box()
 # axis(1,las=1)
--- R/jet.colors.R
+++ R/jet.colors.R
@@ -4,6 +4,7 @@
 #' @return Either a vector of colors or a palette object
 #' example
 #' plot(1:10,1:10, col=jet.colors(palette=T))
+#' @export
 
 jet.colors<-function(palette=F){
 jet.colors <-  c("#00007F", "blue", "#007FFF", "cyan","#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000")
